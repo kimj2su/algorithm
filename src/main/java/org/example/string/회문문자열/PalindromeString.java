7f0042c161c84c08909089b16cb0dd92d1baa227
kim jisu <jisu3316@naver.com>
2023-02-22 23:21:52 +0900

    회문문자열

---
+++ src/main/java/org/example/string/회문문자열/PalindromeString.java
@@ -0,0 +1,46 @@
+package org.example.string.회문문자열;
+
+import org.example.Main;
+
+import java.util.Scanner;
+
+public class PalindromeString {
+
+    public String solution(String str) {
+//        String answer = "YES";
+//        str = str.toUpperCase();
+//        int length = str.length();
+//        for (int i = 0; i < length / 2; i++) {
+//            if (str.charAt(i) != str.charAt(length - i -1 )) {
+//                return  "NO";
+//            }
+//        }
+
+        String answer = "NO";
+        String tmp = new StringBuilder(str).reverse().toString();
+        if (str.equals(tmp)) answer = "YES";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        PalindromeString palindromeString = new PalindromeString();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(palindromeString.solution(str));
+    }
+}
+
+/**
+ * 설명
+ * 앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 회문 문자열이라고 합니다.
+ * 문자열이 입력되면 해당 문자열이 회문 문자열이면 "YES", 회문 문자열이 아니면 “NO"를 출력하는 프로그램을 작성하세요.
+ * 단 회문을 검사할 때 대소문자를 구분하지 않습니다.
+ * 입력
+ * 첫 줄에 길이 100을 넘지 않는 공백이 없는 문자열이 주어집니다.
+ * 출력
+ * 첫 번째 줄에 회문 문자열인지의 결과를 YES 또는 NO로 출력합니다.
+ * 예시 입력 1
+ * gooG
+ * 예시 출력 2
+ * YES
+ */
